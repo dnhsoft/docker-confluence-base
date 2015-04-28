76e2963f7783d6dfebcac8ac8f5e64181c889619
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2015-04-28 19:07:10 +0300

    Safety check added on overriding the data directory

--- 5.x/assets/run.sh
+++ 5.x/assets/run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-mkdir -p /var/atlassian/application-data/confluence
-mv /temp-confuence-data-dir/* /var/atlassian/application-data/confluence
-chown -R confluence:confluence /var/atlassian/application-data/confluence
+CONFLUENCE_DATA_DIR=/var/atlassian/application-data/confluence
+
+mkdir -p $CONFLUENCE_DATA_DIR
+
+if [ "$(ls -A $CONFLUENCE_DATA_DIR)" ]; then
+    echo "Directory $CONFLUENCE_DATA_DIR is not empty and will not be modified"
+else
+    mv /temp-confuence-data-dir/* $CONFLUENCE_DATA_DIR
+    chown -R confluence:confluence $CONFLUENCE_DATA_DIR
+fi
 
 /opt/atlassian/confluence/bin/start-confluence.sh -fg
+
